3b247dc6c78d724d6e6f25349ffa8614458d5f98
Deyvid Spindola <deyvid.spindola@ciandt.com>
2024-01-02 11:23:21 -0300

    Atualizando versões repositorio

--- src/infrastructure/mongodb/mongodb.ts
+++ src/infrastructure/mongodb/mongodb.ts
@@ -1,8 +1,6 @@
 import { MongoClient } from 'mongodb';
 import { Container, Factory, Inject, ObjectFactory } from 'typescript-ioc';
 import { Configurations } from '../configuration/configurations';
-import { Chat } from '../../domain/entities/chat';
-import { ChatStatus } from '../../domain/entities/enums/chat-status';
 
 export const repositoryRegisterStoryFactory: ObjectFactory = () => {
   const config = Container.get(Configurations);
@@ -23,42 +21,7 @@ export class MongoDb {
     console.log('Conectado ao MongoDB');
   }
 
-  async saveChat(chat: Chat) {
-    const collection = this.client.db(this.database).collection('chats');
-    const document = await collection.findOne({ chatId: chat.chatId });
-    if (document) {
-      await collection.updateOne(
-        { chatId: chat.chatId },
-        {
-          $set: {
-            status: chat.status,
-            updatedAt: new Date(),
-            deletedAt: null,
-          },
-        },
-      );
-      return;
-    }
-    await collection.insertOne(chat);
-  }
-
-  async removeChat(chatId: number) {
-    const collection = this.client.db(this.database).collection('chats');
-    await collection.updateOne(
-      { chatId },
-      {
-        $set: {
-          status: ChatStatus.INACTIVE,
-          deletedAt: new Date(),
-          updatedAt: new Date(),
-        },
-      },
-    );
-  }
-
-  async chats(): Promise<Chat[]> {
-    const collection = this.client.db(this.database).collection('chats');
-    const documents = await collection.find().toArray();
-    return documents.map((doc) => doc as unknown as Chat).filter((doc) => doc.status === ChatStatus.ACTIVE);
+  async getClient() {
+    return this.client;
   }
 }
--- src/infrastructure/mongodb/repositories/mongo-chat-repository.ts
+++ src/infrastructure/mongodb/repositories/mongo-chat-repository.ts
@@ -3,25 +3,60 @@ import { MongoDb } from '../mongodb';
 import { Chat } from '../../../domain/entities/chat';
 import { ChatRepository } from '../../../domain/chat-repository';
 import { ChatStatus } from '../../../domain/entities/enums/chat-status';
+import { Configurations } from '../../configuration/configurations';
 
 export class MongoChatRepository extends ChatRepository {
   constructor(
     @Inject
     private readonly mongoDb: MongoDb,
+    @Inject
+    private readonly configuration: Configurations,
   ) {
     super();
   }
+
   async save(chat: Chat) {
-    return await this.mongoDb.saveChat(chat);
+    const client = await this.mongoDb.getClient();
+    const collection = client.db(this.configuration.mongoDbDatabase).collection('chats');
+    const document = await collection.findOne({ chatId: chat.chatId });
+    if (document) {
+      await collection.updateOne(
+        { chatId: chat.chatId },
+        {
+          $set: {
+            status: chat.status,
+            updatedAt: new Date(),
+            deletedAt: null,
+          },
+        },
+      );
+      return;
+    }
+    await collection.insertOne(chat);
   }
   async remove(chatId: number) {
-    await this.mongoDb.removeChat(chatId);
+    const client = await this.mongoDb.getClient();
+    const collection = client.db(this.configuration.mongoDbDatabase).collection('chats');
+    await collection.updateOne(
+      { chatId },
+      {
+        $set: {
+          status: ChatStatus.INACTIVE,
+          deletedAt: new Date(),
+          updatedAt: new Date(),
+        },
+      },
+    );
   }
   async chats(): Promise<Chat[]> {
-    return await this.mongoDb.chats();
+    const client = await this.mongoDb.getClient();
+    const collection = client.db(this.configuration.mongoDbDatabase).collection('chats');
+    const documents = await collection.find().toArray();
+    return documents.map((doc) => doc as unknown as Chat).filter((doc) => doc.status === ChatStatus.ACTIVE);
   }
+
   async exists(chatId: number): Promise<boolean> {
-    const chats = await this.mongoDb.chats();
+    const chats = await this.chats();
     return chats.some((chat) => chat.chatId === chatId && chat.status === ChatStatus.ACTIVE);
   }
 }
